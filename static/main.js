cddc8978d31a04de7ffeeee3672a19c61e375d9c
KrisztianKardos <frekazoid85@gmail.com>
2017-06-27 20:34:30 +0200

    index.html: added residents table; main.js: loadPlanets got refactored for ajax, displayResidents and the modal trigger got included

--- static/main.js
+++ static/main.js
@@ -1,24 +1,25 @@
 function main() {
     var defaultPageNumber = 1
-    apiConnect(defaultPageNumber);
-    var planetsSwapi = apiConnect(defaultPageNumber);
-    fillTable(planetsSwapi);
+    loadPlanets(defaultPageNumber);
 }
 
-
-function apiConnect(apiPageNumber) {
+function loadPlanets(apiPageNumber) {
     var apiUrl = "http://swapi.co/api/planets/?page="
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", apiUrl + apiPageNumber, false);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
-    var response = JSON.parse(xhttp.responseText);
-    return response['results']
+    $.ajax({
+            type: 'GET',
+            url: apiUrl + apiPageNumber,
+            dataType: 'json',
+            success: function(response) {
+                fillTable(response['results']);
+                },
+            error: function() {
+                alert('Error in network request!');
+            } 
+        });
 }
 
 
 function fillTable(planetsSwapi) {
-    //debugger;
     var table = document.getElementById('planetsTable');
     var planetsSwapiLength = planetsSwapi.length;
 
@@ -44,10 +45,14 @@ function fillTable(planetsSwapi) {
         row.appendChild(cellResidents);
 
         var name = planetsSwapi[i]['name'];
-        var diameter = planetsSwapi[i]['diameter'];
+        var diameter = planetsSwapi[i]['diameter'] / 1000 + ' km';
         var climate = planetsSwapi[i]['climate'];
         var terrain = planetsSwapi[i]['terrain'];
-        var surfaceWater = planetsSwapi[i]['surface_water'] + ' %';
+        if ( planetsSwapi[i]['surface_water'] == 'unknown' ) {
+            surfaceWater = planetsSwapi[i]['surface_water'];
+            } else {
+                surfaceWater = planetsSwapi[i]['surface_water'] + ' %';
+            }
         var population = planetsSwapi[i]['population'];
         if (planetsSwapi[i]['residents'].length == 0) {
             var residents = 'No known residents';
@@ -75,6 +80,61 @@ function fillTable(planetsSwapi) {
         residentsLink.setAttribute('data-target', '#residentsModal');
         cellResidents.appendChild(residentsLink);
     }
+
+    //modal trigger for residents
+    $('#residentsModal').on('show.bs.modal', function (event) {
+        
+        var button = $(event.relatedTarget);
+        var planetName = button.data('planetname');
+        var residents = button.data('residents');
+        var modal = $(this);
+        var residentsSwapiLinks = residents.split(",");
+
+        modal.find('.modal-title').text('Residents of ' + planetName);
+        for (let i = 0; i < residentsSwapiLinks.length; i++) {
+            $.ajax({
+                type: 'GET',
+                url: residentsSwapiLinks[i],
+                dataType: 'json',
+                success: function(response) {
+                    console.log(response)
+                    displayResidents(response);
+                    },
+                error: function() {
+                    alert('Error in network request!');
+                } 
+            });
+        }
+    });
+
+function displayResidents(residents) {
+            var name = residents['name'];
+            var height = residents['height'] / 100 + ' m';
+            if ( residents['mass'] == 'unknown' ) {
+                mass = residents['mass'];
+                } else {
+                    mass = residents['mass'] + ' kg';
+                }
+            var hair_color = residents['hair_color'];
+            var skin_color = residents['skin_color'];
+            var eye_color = residents['eye_color'];
+            var birth_year = residents['birth_year'];
+            var gender = residents['gender'];
+            $('#residentsTable').append('<tr>' +
+                                        '<td>' + name + '</td>' +
+                                        '<td>' + height + '</td>' +
+                                        '<td>' + mass + '</td>' +
+                                        '<td>' + hair_color + '</td>' +
+                                        '<td>' + skin_color + '</td>' +
+                                        '<td>' + eye_color + '</td>' +
+                                        '<td>' + birth_year + '</td>' +
+                                        '<td>' + gender + '</td>' +
+                                        '</tr>');
+        }
+
+    $('#residentsModal').on('hidden.bs.modal', function () {
+        $(this).find('.modal-body').text('');
+    });
 }
 
 $(document).ready(main());
